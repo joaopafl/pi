b1c233411583dcffc819c8e711a510893bbd87fe
Joao Paulo <joaopaf@gmail.com>
2025-07-08 15:49:25 -0300

    Adicionando Projeto do Joao

---
+++ Projeto_MVC_Produtos/Projeto_MVC_Produtos/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_MVC_Produtos.Models;
+
+namespace Projeto_MVC_Produtos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Fornecedor> Fornecedores { get; set;}
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ Projeto_MVC_Produtos/Projeto_MVC_Produtos/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace Projeto_MVC_Produtos.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
